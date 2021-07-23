8f816aefb85c9c1f8decc8a397000788abe106e9
Mark Alford Jr <markalfordjr@gmail.com>
2021-07-23 14:32:41 -0400

    Attempt in adding Constant file to git ignore

---
+++ AssignmentOne/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  AssignmentOne
+//
+//  Created by Mark Alford on 7/23/21.
+//
+
+import Foundation
+
+struct Constant{
+    let privateApiKey = "08ee85dd3bb64105bec195503212906"
+}
